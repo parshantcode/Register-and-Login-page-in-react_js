5844c11195e3fb5059f12d3bc9fbcf6baec39f20
Parshant kumar <parshantsingh7464@gmail.com>
2024-09-27 01:11:25 +0530

    commit

---
+++ client/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+function Home() {
+    return(
+       <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+       <h1> welcome to Home Page</h1> 
+       </div>
+    )
+}
+
+export default Home;
